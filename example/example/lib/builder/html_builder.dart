8cc76a808a307b9c50cf41ca91816d51582d8a6e
t4ehye0ng <t4ehye0ng@gmail.com>
2020-05-11 10:10:05 +0900

    Add html_builder

---
+++ example/example/lib/builder/html_builder.dart
@@ -0,0 +1,55 @@
+/* This is free and unencumbered software released into the public domain. */
+
+import 'package:flutter/material.dart';
+
+abstract class HTMLBuilder {
+  HTMLBuilder._();
+
+  static String build(
+      {@required final String htmlTemplate,
+      @required final Color backgroundColor,
+      @required final String src,
+      final String alt,
+      final bool ar,
+      final List<String> arModes,
+      final String arScale,
+      final bool autoRotate,
+      final int autoRotateDelay,
+      final bool autoPlay,
+      final bool cameraControls,
+      final String iosSrc}) {
+    final html = StringBuffer(htmlTemplate);
+    html.write('<model-viewer');
+    html.write(' style="background-color: rgb(${backgroundColor.red}, ${backgroundColor.green}, ${backgroundColor.blue});"');
+    html.write(' src="$src"');
+    if (alt != null) {
+      html.write(' alt="$alt"'); // TODO: escape string
+    }
+    if (ar ?? false) {
+      html.write(' ar');
+    }
+    if (arModes != null) {
+      html.write(' ar-modes="${arModes.join(' ')}"'); // TODO: escape string
+    }
+    if (arScale != null) {
+      html.write(' ar-scale="$arScale"'); // TODO: escape string
+    }
+    if (autoRotate ?? false) {
+      html.write(' auto-rotate');
+    }
+    if (autoRotateDelay != null) {
+      html.write(' auto-rotate-delay="$autoRotateDelay"');
+    }
+    if (autoPlay ?? false) {
+      html.write(' autoplay');
+    }
+    if (cameraControls ?? false) {
+      html.write(' camera-controls');
+    }
+    if (iosSrc != null) {
+      html.write(' ios-src="$iosSrc"'); // TODO: escape string
+    }
+    html.writeln('></model-viewer>');
+    return html.toString();
+  }
+}
--- example/example/lib/main.dart
+++ example/example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'builder/html_builder.dart';
 
 void main() => runApp(MyApp());
 
